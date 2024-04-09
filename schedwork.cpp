1543af44873f38a8d6cd9deaf0ca85df3ed961a8
David Cue <dcue@usc.edu>
2024-04-09 19:15:39 +0000

    initial commit

--- schedwork.cpp
+++ schedwork.cpp
@@ -1,6 +1,5 @@
 #ifndef RECCHECK
 #include <set>
-#include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
@@ -8,38 +7,107 @@
 #include <algorithm>
 // add or remove necessary headers as you please
 
+
 #endif
+
 
 #include "schedwork.h"
 
+
 using namespace std;
 
-// a constant that can be used to indicate an INVALID 
+
+// a constant that can be used to indicate an INVALID
 // worker ID if that is useful to your implementation.
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
+
+
 // Add prototypes for any helper functions here
-
-
+bool scheduleHelp(
+const AvailabilityMatrix& avail,
+   const size_t dailyNeed,
+   const size_t maxShifts,
+   DailySchedule& sched
+);
+bool hasShifts(DailySchedule& sched, int worker, const size_t maxShifts);
+bool workingToday(DailySchedule& sched, int worker, const size_t maxShifts, int r);
 // Add your implementation of schedule() and other helper functions here
+bool backtrack(const AvailabilityMatrix& avail,const size_t dailyNeed,const size_t maxShifts,DailySchedule& sched, int r, int c);
+
 
 bool schedule(
-    const AvailabilityMatrix& avail,
-    const size_t dailyNeed,
-    const size_t maxShifts,
-    DailySchedule& sched
+   const AvailabilityMatrix& avail,
+   const size_t dailyNeed,
+   const size_t maxShifts,
+   DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
-        return false;
-    }
-    sched.clear();
-    // Add your code below
+   if(avail.size() == 0U){
+       return false;
+   }
+   sched.clear();
+   // Add your code below
 
 
-
-
+   return scheduleHelp(avail,dailyNeed,maxShifts,sched);
 }
 
+
+bool scheduleHelp(const AvailabilityMatrix& avail,const size_t dailyNeed,const size_t maxShifts,DailySchedule& sched){
+ size_t days = avail.size();
+ size_t workers = avail[0].size();
+   if (days == 0 || workers == 0 || dailyNeed == 0){
+     return false;
+   }
+
+
+   sched.resize(days, vector<Worker_T>(dailyNeed, INVALID_ID));
+   return backtrack(avail, dailyNeed, maxShifts, sched, 0, 0);
+}
+
+
+bool hasShifts(DailySchedule& sched, int worker, const size_t maxShifts){
+ int shifts=0;
+ for(vector<vector<Worker_T>>::iterator it = sched.begin(); it!=sched.end(); it++){
+   vector<Worker_T> curr=*it;
+   for(vector<Worker_T>::iterator wor = curr.begin(); wor!=curr.end(); wor++){
+     if(worker==*wor){
+             shifts++;
+               if (shifts >= maxShifts){
+                return false;
+               }
+       }
+   }
+ }
+ return true;
+}
+
+
+bool workingToday(DailySchedule& sched, int worker, const size_t maxShifts, int r){
+ vector<Worker_T> curr=sched[r];
+  if(find(curr.begin(),curr.end(),worker)!=curr.end()){return true;}
+   return false;
+}
+
+
+bool backtrack(const AvailabilityMatrix& avail,const size_t dailyNeed,const size_t maxShifts,DailySchedule& sched, int r, int c){
+ size_t workers=avail[0].size();
+ if (r>= avail.size()) {
+       return true;
+   }
+   for(int i=0; i<workers; i++){
+     if(avail[r][i] && hasShifts(sched,i,maxShifts) && !workingToday(sched,i,maxShifts,r)){
+         sched[r][c] = i;
+         if(c+1==dailyNeed){
+           if(backtrack(avail,dailyNeed,maxShifts,sched,r+1,0)){return true;}
+         }else{
+           if(backtrack(avail,dailyNeed,maxShifts,sched,r,c+1)){return true;}
+         }
+     }
+     sched[r][c]=INVALID_ID;
+   }
+ return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -2,27 +2,97 @@
 // For debugging
 #include <iostream>
 // For std::remove
-#include <algorithm> 
+#include <algorithm>
 #include <map>
 #include <set>
 #endif
+
 
 #include "wordle.h"
 #include "dict-eng.h"
 using namespace std;
 
 
+
+
 // Add prototypes of helper functions here
-
-
+void backTrackHelper(const std::string& in,const std::string& floating,const std::set<std::string>& dict, std::string currW, std::set<std::string>& words, int free);
+bool isValid(const std::string& in,const std::string& floating,const std::set<std::string>& dict, std::string currW, std::set<std::string>& words);
 // Definition of primary wordle function
 std::set<std::string> wordle(
-    const std::string& in,
-    const std::string& floating,
-    const std::set<std::string>& dict)
+   const std::string& in,
+   const std::string& floating,
+   const std::set<std::string>& dict)
 {
-    // Add your code here
+   // Add your code here
+   std::string currW="";
+   std::set<std::string>* words = new std::set<std::string>();
+   int req = floating.size(); //1
+   int dCount = 0;
+   for(int i=0; i<in.size(); i++){ //1
+     if(in.at(i)=='-'){dCount++;}
+   }
 
+
+   backTrackHelper(in, floating, dict, currW, *words, dCount-req);
+   return *words;
 }
 
+
 // Define any helper functions here
+ void backTrackHelper(const std::string& in,const std::string& floating,const std::set<std::string>& dict, std::string currW, std::set<std::string>& words, int free){
+   if(isValid(in,floating,dict,currW,words)){
+     words.insert(currW);
+     return;
+   }
+   if(currW.size()>=in.size()){return;}
+   if(in.at(currW.size())=='-'){
+   for(char c='a'; c<='z'; c++){
+     if(free==0 && (floating.find(c) == std::string::npos)){}
+     else if(free<0){return;}
+     else{
+     currW+=c;
+     bool minus=false;
+     if(floating.find(c) == std::string::npos){free--; minus=true;}
+     backTrackHelper(in,floating,dict,currW,words,free);
+     if(minus){free++;}
+     if(currW.size()!=0){currW.pop_back();} }
+     }
+   }else{
+     currW+=in.at(currW.size());
+     backTrackHelper(in,floating,dict,currW,words,free);
+     currW.pop_back();
+   }
+}
+
+
+   bool isValid(const std::string& in,const std::string& floating,const std::set<std::string>& dict, std::string currW, std::set<std::string>& words){
+     if(currW.size()!=in.size()){return false;}
+     //create a map from letter to number, each time you search
+
+
+     if(dict.find(currW)!=dict.end()){
+       map<char,int> req;
+       for(int i=0; i<floating.length();i++){
+         char ch= floating.at(i);
+         map<char,int>::iterator it = req.find(ch);
+         if(it==req.end()){req.insert(std::pair<char,int>(ch,1) );}
+         else{it->second++;}
+       }
+
+
+       for(int i=0; i<currW.length(); i++){
+         map<char,int>::iterator it = req.find(currW.at(i));
+            if(it!=req.end()){
+             if(it->second>0){
+                 it->second--;
+             }
+              
+            }
+       }
+       for (auto i = req.begin(); i != req.end(); i++){
+       if(i->second!=0){return false;}}
+       return true;
+     }
+     return false;
+   }
